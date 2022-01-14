fc5dfc08e16ff35dca93cc1872ae4aa9fd27a3c3
ppodds <oscar20020629@gmail.com>
2022-01-14 20:02:42 +0800

    Feat: avatar command

---
+++ src/core/command-handler/commands/basic/avatar.ts
@@ -0,0 +1,31 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { info } from "../../../graphics/embeds";
+import { CommandInterface } from "../../CommandInterface";
+
+const command: CommandInterface = {
+    data: new SlashCommandBuilder()
+        .setName("avatar")
+        .setDescription("取得目標的Discord頭像(無目標則獲得自己的頭像)")
+        .addUserOption((option) =>
+            option.setName("target").setDescription("目標使用者")
+        ),
+    async execute(interaction) {
+        const embed = info(interaction.client, "「看來這就是你要的呢...」");
+        const target = interaction.options.getUser("target");
+        if (target)
+            embed.setImage(
+                target.avatarURL({ dynamic: true, format: "png", size: 1024 })
+            );
+        else
+            embed.setImage(
+                interaction.user.avatarURL({
+                    dynamic: true,
+                    format: "png",
+                    size: 1024,
+                })
+            );
+        await interaction.reply({ embeds: [embed] });
+    },
+};
+
+export = command;
