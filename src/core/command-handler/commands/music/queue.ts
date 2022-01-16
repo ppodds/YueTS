ac7e12695f260f2d013eaea937bdc24dd173ab19
ppodds <oscar20020629@gmail.com>
2022-01-17 01:56:54 +0800

    Feat: add queue

---
+++ src/core/command-handler/commands/music/queue.ts
@@ -0,0 +1,89 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import {
+    paginationEmbed,
+    info,
+    paginationButton,
+} from "../../../graphics/embeds.js";
+import PlayerManager from "../../../music/PlayerManager.js";
+import { CommandInterface } from "../../CommandInterface.js";
+
+const command: CommandInterface = {
+    data: new SlashCommandBuilder()
+        .setName("queue")
+        .setDescription("觀看接下來歌曲的順序"),
+    async execute(interaction) {
+        const user = interaction.member;
+
+        if (!user)
+            return await interaction.reply("似乎在私聊時不能做這些呢....");
+        else if (!PlayerManager.exist(interaction.guild))
+            return await interaction.reply("嗯? 我沒有在唱歌喔~");
+
+        const musicPlayer = PlayerManager.get(interaction);
+        const queue = musicPlayer.getQueue();
+
+        function generateEmbed() {
+            const embed = info(
+                interaction.client,
+                "「目前已經有這麼多人和Yue點歌了呢... Yue有點小高興...」"
+            );
+
+            embed.addFields(
+                {
+                    name: "撥放佇列狀態",
+                    value: `目前撥放佇列中有 ${queue.length} 首歌曲`,
+                    inline: false,
+                },
+                {
+                    name: "格式範例",
+                    value: "「待會Yue就用這種方式照著念喔~」",
+                    inline: false,
+                },
+                {
+                    name: "順序",
+                    value: "歌曲名稱",
+                    inline: false,
+                }
+            );
+            return embed;
+        }
+
+        // don't need paginationEmbed
+        if (queue.length <= 22) {
+            const embed = generateEmbed();
+            for (const i in queue) {
+                embed.addField(
+                    (parseInt(i) + 1).toString(),
+                    queue[i].metadata.videoDetails.title
+                );
+            }
+            await interaction.reply({ embeds: [embed] });
+        } else {
+            // generate pages
+            let i = 0;
+            const pagesData = [];
+            while (i < queue.length) {
+                pagesData.push(queue.slice(i, (i += 22)));
+            }
+
+            const pages = [];
+            let count = 0;
+            pagesData.forEach((pageData) => {
+                const embed = generateEmbed();
+
+                for (const track of pageData) {
+                    embed.addField(
+                        (count += 1).toString(),
+                        track.metadata.videoDetails.title
+                    );
+                }
+
+                pages.push(embed);
+            });
+
+            await paginationEmbed(interaction, pages, paginationButton());
+        }
+    },
+};
+
+export default command;
