1d529f7b56fbb125ffe00d4443c5e9cdcc3fefe8
ppodds <oscar20020629@gmail.com>
2022-01-17 01:56:53 +0800

    Fix: CommandInterface init return wrong type

--- src/core/command-handler/CommandInterface.ts
+++ src/core/command-handler/CommandInterface.ts
@@ -1,9 +1,8 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/rest/v9";
-import { ApplicationCommandData, Client, CommandInteraction } from "discord.js";
+import { Client, CommandInteraction } from "discord.js";
 
 export interface CommandInterface {
     data: Omit<SlashCommandBuilder, any>;
-    init?(client: Client, name: string): Promise<ApplicationCommandData[]>;
+    init?(client: Client, name: string): Promise<void>;
     execute(interaction: CommandInteraction): Promise<void>;
 }
