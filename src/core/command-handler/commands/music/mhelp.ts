64f2318e6e373483a12216dcc3012b7f791e915a
ppodds <oscar20020629@gmail.com>
2022-01-17 01:56:54 +0800

    Feat: add mhelp

---
+++ src/core/command-handler/commands/music/mhelp.ts
@@ -0,0 +1,60 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { info } from "../../../graphics/embeds.js";
+import { CommandInterface } from "../../CommandInterface.js";
+
+const command: CommandInterface = {
+    data: new SlashCommandBuilder()
+        .setName("mhelp")
+        .setDescription("觀看Yue的音樂指令說明"),
+    async execute(interaction) {
+        const embed = info(
+            interaction.client,
+            "「想聽Yue唱歌嗎? 下次說不定有機會呢~~」"
+        );
+        embed.addFields(
+            {
+                name: "play",
+                value: "讓Yue唱Youtube有的歌曲",
+                inline: false,
+            },
+            {
+                name: "join",
+                value: "讓Yue加入你所在的頻道",
+                inline: false,
+            },
+            {
+                name: "queue",
+                value: "觀看接下來歌曲的順序",
+                inline: false,
+            },
+            {
+                name: "playing",
+                value: "觀看正在撥放中的歌曲資訊",
+                inline: false,
+            },
+            {
+                name: "pause",
+                value: "讓Yue暫停唱歌",
+                inline: false,
+            },
+            {
+                name: "resume",
+                value: "讓Yue繼續唱歌",
+                inline: false,
+            },
+            {
+                name: "skip",
+                value: "讓Yue跳過當前正在唱的歌",
+                inline: false,
+            },
+            {
+                name: "stop",
+                value: "讓Yue離開 並清空預計要唱的歌曲",
+                inline: false,
+            }
+        );
+        await interaction.reply({ embeds: [embed] });
+    },
+};
+
+export default command;
