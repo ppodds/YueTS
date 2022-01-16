d1bca44b63e469896529a5b7734448ff7165f0b5
ppodds <oscar20020629@gmail.com>
2022-01-17 01:56:54 +0800

    Feat: add resume

---
+++ src/core/command-handler/commands/music/resume.ts
@@ -0,0 +1,29 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import PlayerManager from "../../../music/PlayerManager.js";
+import { AudioPlayerStatus } from "@discordjs/voice";
+import { CommandInterface } from "../../CommandInterface.js";
+
+const command: CommandInterface = {
+    data: new SlashCommandBuilder()
+        .setName("resume")
+        .setDescription("讓Yue繼續唱歌"),
+    async execute(interaction) {
+        const user = interaction.member;
+
+        if (!user)
+            return await interaction.reply("似乎在私聊時不能做這些呢....");
+        else if (!PlayerManager.exist(interaction.guild))
+            return await interaction.reply("嗯? 我沒有在唱歌喔~");
+
+        const musicPlayer = PlayerManager.get(interaction);
+        if (musicPlayer.getPlayerStatus() !== AudioPlayerStatus.Paused)
+            return await interaction.reply(
+                "我現在已經在唱歌了啦 <:i_yoshino:583658336054935562>"
+            );
+
+        musicPlayer.resume();
+        await interaction.reply("那我就繼續唱哦....");
+    },
+};
+
+export default command;
