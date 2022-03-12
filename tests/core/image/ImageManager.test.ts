1fdacfebdf45e2b47d993f64f31e955d87adcb7d
ppodds <oscar20020629@gmail.com>
2022-03-12 23:30:11 +0800

    Fix: update package cause test data not fit

--- tests/core/image/ImageManager.test.ts
+++ tests/core/image/ImageManager.test.ts
@@ -7,7 +7,7 @@ test("the test image phash should equal", async () => {
     const phash = await imageManager.makePhash(await fs.readFile(TESTFILE));
 
     expect(phash).toEqual(
-        "0111011111100100110011001000110011001111100110011001100110000011"
+        "0111010111100100110010001000110011001111100110011001100110000011"
     );
 });
 
