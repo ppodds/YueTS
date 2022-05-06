638fd4bdd80cf65e80077add9942867a359bbf73
ppodds <oscar20020629@gmail.com>
2022-05-06 19:28:01 +0800

    Refactor: message.ts

--- src/core/graphics/message.ts
+++ src/core/graphics/message.ts
@@ -2,17 +2,17 @@ import { TextChannel } from "discord.js";
 
 /**
  * Send a message to text channel and delete it after delay
- * @param {Channel} channel the channel where you want to send message
- * @param {string} content message content
- * @param {number} deleteAfter delete delay (ms)
+ * @param channel the channel where you want to send message
+ * @param content message content
+ * @param deleteAfter delete delay (ms)
  */
 export async function send(
     channel: TextChannel,
     content: string,
     deleteAfter: number
 ) {
-    const message = await channel.send(content);
     if (deleteAfter < 0) throw new Error("only accept number > 0");
     else if (deleteAfter === 0) return;
+    const message = await channel.send(content);
     setTimeout(async () => await message.delete(), deleteAfter);
 }
