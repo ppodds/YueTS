671afe1fbe3b10f1a8c78cfdc3f956f174ea0e1d
ppodds <oscar20020629@gmail.com>
2022-12-25 18:38:46 +0800

    fix: guard function works improperly

--- src/guards/GuildOnly.ts
+++ src/guards/GuildOnly.ts
@@ -6,13 +6,16 @@ export const GuildOnly: GuardFunction<CommandInteraction> = async (
     client,
     next
 ) => {
-    if (interaction.inGuild()) await next();
-    const content = "這個指令只能在伺服器中使用";
-    if (interaction.deferred) {
-        await interaction.editReply(content);
-    } else if (interaction.replied) {
-        await interaction.followUp(content);
-    } else {
-        await interaction.reply(content);
+    if (!interaction.inGuild()) {
+        const content = "這個指令只能在伺服器中使用";
+        if (interaction.deferred) {
+            await interaction.editReply(content);
+        } else if (interaction.replied) {
+            await interaction.followUp(content);
+        } else {
+            await interaction.reply(content);
+        }
+        return;
     }
+    await next();
 };
